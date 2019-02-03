957ca1edf135158324acb93a12b853ac304de624
赵威杰 <1060656096@qq.com>
2019-02-03 21:36:28 +0800

    aaa: 增加文档示例

---
+++ example_test.go
@@ -0,0 +1,18 @@
+package env
+
+import "fmt"
+
+func ExampleGet() {
+	fmt.Println("hello")
+	// Output: hello
+	// haha
+}
+
+func ExampleDemo() {
+	fmt.Println("示例输出1")
+	fmt.Println("示例输出2")
+
+	// Output:
+	// 示例输出1
+	// 示例输出2
+}
